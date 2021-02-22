f6f5926ab59d75fddaae442d1d66a87333b0ea1f
Yuanyuan Gao <32939544+YuanyuanGao216@users.noreply.github.com>
2021-02-22 12:24:34 -0500

    create figs

--- SimulateRestingData.m
+++ SimulateRestingData.m
@@ -15,7 +15,8 @@ simulated_HbR = [];
 DataDir = '../New Datasets/05/motion_artifacts_1';
 Files = dir(fullfile(DataDir,'*.nirs'));
 figure_flag = 0;
-for file = 1:length(Files) - 1 % save the last one for testing
+% for file = 2:length(Files) - 1 % save the last one for testing
+for file = 2:2 % save the last one for testing
     name = Files(file).name;
     fprintf('fnirs file is %s\n',name);
     fNIRS_data = load([DataDir,'/',name],'-mat');
@@ -39,52 +40,54 @@ for file = 1:length(Files) - 1 % save the last one for testing
     %% select period without MA and fit AR(5) model, simulate new data
     n_Ch        =   size(dc,3);
     for bio = 1:2
-        for Ch = 1:n_Ch
+%         for Ch = 1:n_Ch
+        for Ch = 1:1
             % exclude the noisy channel
             if SD.MeasListAct(Ch) == 0
                 continue
             end
             % scan along the time line
             period = 512;
-            for start_point = 1:1:(length(tInc) - period)
+            for start_point = 1:1000:(length(tInc) - period)
                 t_span = start_point : start_point + period -1;
                 % if contains noise, skip
                 if any(tInc(t_span)==0)
                     continue
                 end
                 % try 10 times AR simulation
-                for n = 1:10
+                for n = 1:1
                     Resting_HbO = squeeze(dc(t_span,bio,Ch));
-                    [sim_data_HbO,state] = AR_sim(Resting_HbO);
-                    % if the amplitude of sim is 30 times larger, skip
-                    if abs(max(sim_data_HbO)) > abs(max(Resting_HbO)) * 30 || state == 1
-                        continue
-                    end
+                    
+%                     [sim_data_HbO,state] = AR_sim(Resting_HbO);
+%                     % if the amplitude of sim is 30 times larger, skip
+%                     if abs(max(sim_data_HbO)) > abs(max(Resting_HbO)) * 30 || state == 1
+%                         continue
+%                     end
+                    
                     % plot an example
-                    if figure_flag == 0 && Ch == channel
-                        plot_real_data(t,dc,tInc,channel,t_span,Resting_HbO,sim_data_HbO)
-                        save('Processed_data/plot_Resting.mat','t','dc','tInc','channel','t_span','Resting_HbO','sim_data_HbO');
-                    end
-                    % if simulate successfully, record in simulated_HbO and _HbR
-                    if bio == 1
-                        if figure_flag == 0
-                            figure_flag = 1;
-                            loc_example = size(simulated_HbO,1) + 1;
-                        end
-                        simulated_HbO(end+1,:) = sim_data_HbO;
-                    else
-                        simulated_HbR(end+1,:) = sim_data_HbO;
+                    if file == 2 && Ch == 1
+                          plot_real_data(t,dc,tInc,Ch)
+%                         plot_real_data(t,dc,tInc,channel,t_span,Resting_HbO,sim_data_HbO)
+%                         figure_flag = 1;
+%                         loc_example = size(simulated_HbO,1) + 1;
+%                         save('Processed_data/plot_Resting.mat','t','dc','tInc','channel','t_span','Resting_HbO','sim_data_HbO','loc_example');
                     end
                     
-                    break
+                    % if the amplitude is ok, we save the data and stop the simulation
+%                     if bio == 1
+%                         simulated_HbO(end+1,:) = sim_data_HbO;
+%                     else
+%                         simulated_HbR(end+1,:) = sim_data_HbO;
+%                     end
+%                     break
                 end
             end
         end
     end
 end
-fprintf('%d samples in HbO\n', size(simulated_HbO,1))
-fprintf('%d samples in HbR\n', size(simulated_HbR,1))
-
-save('Processed_data/sim_Resting.mat','simulated_HbO','simulated_HbR', 'loc_example')
+% fprintf('%d samples in HbO\n', size(simulated_HbO,1))
+% fprintf('%d samples in HbR\n', size(simulated_HbR,1))
+% 
+% save('Processed_data/sim_Resting.mat','simulated_HbO','simulated_HbR', 'loc_example')
 
 
--- plot_real_data.m
+++ plot_real_data.m
@@ -1,4 +1,5 @@
-function plot_real_data(t,dc,tInc,ch,t_span,Resting_HbO,sim_data_HbO)
+% function plot_real_data(t,dc,tInc,ch,t_span,Resting_HbO,sim_data_HbO)
+function plot_real_data(t,dc,tInc,ch)
 if (nargin == 0)
     load('Processed_data/plot_Resting.mat');
     ch = channel;
@@ -8,8 +9,8 @@ global fs_new
 fs_new = 7;
 
 dc              =   dc * 1e6;
-Resting_HbO     =   Resting_HbO * 1e6;
-sim_data_HbO    =   sim_data_HbO * 1e6;
+% Resting_HbO     =   Resting_HbO * 1e6;
+% sim_data_HbO    =   sim_data_HbO * 1e6;
 
 figure
 %% plot the real data
@@ -26,34 +27,34 @@ if size(start_p) ~=0
             'edgecolor','none','FaceAlpha',.3);
     end
 end
-legend([h p(1)],'\DeltaHbO','Motion artifact')
+% legend([h p(1)],'\DeltaHbO','Motion artifact')
 title('Experimental data example')
 ylabel('\muMol');xlabel('Time(s)');xlim([min(t) max(t)])
 set(gca,'fontname','Arial','fontsize',10)
-%% plot the motion artifact
-subplot(3,3,4)
-hold on
-x = t(start_p(2):end_p(2));
-plot(x,dc(start_p(2):end_p(2)),'b-','linewidth',1);
-title('Motion artifact')
-ylabel('\muMol');xlabel('Time(s)');xlim([min(x) max(x)])
-set(gca,'fontname','Arial','fontsize',10)
-%% plot the resting data
-subplot(3,2,4)
-hold on
-plot(t_span,Resting_HbO,'b-','linewidth',1);
-title('Resting state data')
-ylabel('\muMol');xlabel('Time(s)');xlim([min(t_span) max(t_span)])
-set(gca,'fontname','Arial','fontsize',10)
-%% plot the simulated data
-subplot(3,1,3)
-hold on
-x = (1:length(sim_data_HbO))./fs_new;
-plot(x,sim_data_HbO,'g-','linewidth',1)
-title('Simulated resting state data')
-ylabel('\muMol');xlabel('Time(s)');xlim([min(x) max(x)])
-set(gca,'fontname','Arial','fontsize',10)
-
-set(gcf,'position',[17    324    1101    504])
-save('Figures/Resting_data_example.fig')
-save('Figures/Resting_data_example.svg')
+% %% plot the motion artifact
+% subplot(3,3,4)
+% hold on
+% x = t(start_p(2):end_p(2));
+% plot(x,dc(start_p(2):end_p(2)),'b-','linewidth',1);
+% title('Motion artifact')
+% ylabel('\muMol');xlabel('Time(s)');xlim([min(x) max(x)])
+% set(gca,'fontname','Arial','fontsize',10)
+% %% plot the resting data
+% subplot(3,2,4)
+% hold on
+% plot(t_span,Resting_HbO,'b-','linewidth',1);
+% title('Resting state data')
+% ylabel('\muMol');xlabel('Time(s)');xlim([min(t_span) max(t_span)])
+% set(gca,'fontname','Arial','fontsize',10)
+% %% plot the simulated data
+% subplot(3,1,3)
+% hold on
+% x = (1:length(sim_data_HbO))./fs_new;
+% plot(x,sim_data_HbO,'g-','linewidth',1)
+% title('Simulated resting state data')
+% ylabel('\muMol');xlabel('Time(s)');xlim([min(x) max(x)])
+% set(gca,'fontname','Arial','fontsize',10)
+% 
+% set(gcf,'position',[17    324    1101    504])
+% save('Figures/Resting_data_example.fig')
+% save('Figures/Resting_data_example.svg')
